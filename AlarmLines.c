76506dd5e501a716fdb9583e81f9bff47e755330
BarmenEditor <158438006+BarmenEditor@users.noreply.github.com>
2024-02-01 14:43:57 +0700

    Add files via upload

--- AlarmLines.c
+++ AlarmLines.c
@@ -4,36 +4,27 @@
 #define HEX_INTERPRETER 16777216
 #define SYMBOL_SHIFT 256
 #define BREAK_LINE_SYMBOL 127
-#define ARRAY_STRING_SIZE 8
+#define ARRAY_STRING_SIZE 16
 #define STRING_SIZE 4
+
+long DataOut[ARRAY_STRING_SIZE][STRING_SIZE];
 
 void Init(void)
 {
-v->String0 = (long)&v->ParO;
-v->String1 = (long)&v->ParT;
-v->String2 = (long)&v->ParTr;
-v->String3 = (long)&v->ParF;
-v->String4 = (long)&v->ParFi;
-v->String5 = (long)&v->ParS;
-v->String6 = (long)&v->ParSe;
-v->String7 = (long)&v->ParEi;
+short i;
+for(i=0;i<ARRAY_STRING_SIZE;i++)
+  {
+    (&v->String0)[i] = (long)&DataOut[i];
+  }
+
 }
+
 
 void Exec(void)
 {
   short z,j=0,i,numExit=0, DataCont=0;
   long bintrans=*v->MaskBin, hexator=HEX_INTERPRETER;
   long *DataInter = (long *)*v->Alphabet;
-  long *DataOut[ARRAY_STRING_SIZE];
-  DataOut[0] = &v->ParO;
-  DataOut[1] = &v->ParT;
-  DataOut[2] = &v->ParTr;
-  DataOut[3] = &v->ParF;
-  DataOut[4] = &v->ParFi;
-  DataOut[5] = &v->ParS;
-  DataOut[6] = &v->ParSe;
-  DataOut[7] = &v->ParEi;
-
   for (i=0;i<ARRAY_STRING_SIZE;i++)
     {
     for (j=0;j<STRING_SIZE;j++)
